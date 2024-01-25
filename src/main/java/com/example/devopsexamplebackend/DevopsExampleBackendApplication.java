435d826db6ab3aff98d2f9658ead46ef263762b9
Anour <anwarnor@hotmail.com>
2024-01-25 21:07:07 +0000

    Initial commit

---
+++ src/main/java/com/example/devopsexamplebackend/DevopsExampleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsexamplebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsExampleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsExampleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devopsexamplebackend/DevopsExampleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsexamplebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsExampleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
