97d83ac9af059cf49f4aa0cbef52620e103469bf
Anour <anwarnor@hotmail.com>
2024-01-25 21:17:39 +0000

    Neue Java Datei hinzugefügt

---
+++ src/main/java/com/example/devopsexamplebackend/MainController.java
@@ -0,0 +1,13 @@
+package com.example.devopsexamplebackend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, DevOps World!";
+    }
+}
